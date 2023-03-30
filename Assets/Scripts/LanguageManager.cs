e27cd75227f24d9178d61d5e241a88ac01bec130
Emerson Mistico <102876556+Emerson-Mistico@users.noreply.github.com>
2023-03-30 20:16:17 -0300

    Added language change with flags
    Added the LanguageManager and change through the flags in the main menu.

---
+++ Assets/Scripts/LanguageManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+
+public class LanguageManager : MonoBehaviour
+{
+    public static LanguageManager instance;
+
+    private bool changeActive = false;
+
+    IEnumerator SetLocale(int _localeID)
+    {
+        changeActive = true;       
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[_localeID];
+        changeActive = false;
+    }
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    public void ChangeLocale(int localeID)
+    {
+        if (changeActive) {
+            return;
+        }
+        StartCoroutine(SetLocale(localeID));
+    }
+}
